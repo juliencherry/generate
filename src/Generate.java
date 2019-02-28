f9d0bcf94eee4075ea7d7dd26ca376766ecb4a15
Julien Cherry <me@juliencherry.net>
2019-02-28 16:49:14 -0500

    Remove unnecessary semicolon

--- src/Generate.java
+++ src/Generate.java
@@ -15,7 +15,7 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
